5d38d028ff0bb1e9f9f4af06c8e7d026fc61e9ad
Timothy Zhang <timothyzhang6@gmail.com>
2018-08-05 14:17:03 -0400

    Revamped multiplayer

--- multiplayer/ExpressServer.js
+++ multiplayer/ExpressServer.js
@@ -1,4 +1,4 @@
-var
+/*var
 	gameport = process.env.PORT||4004,
 	
 	io = require('socket.io'),
@@ -9,6 +9,9 @@ var
 	
 	verbose = false,
 	app = express();
+
+
+app.use(express.static(path.resolve(__dirname + '/../src/')));
 	
 app.listen( gameport );
 
@@ -28,3 +31,35 @@ app.get('/*', function( req, res, next){
 	res.sendFile(path.resolve(__dirname+'/'+ file));
 });
 
+
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});*/
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require('path');
+var UUID = require('uuid');
+
+app.use(express.static(path.resolve(__dirname + '/public/')));
+
+var gameport = 4004;
+server.listen(gameport);
+console.log('\t :: Express :: Listening on port ' + gameport);
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/public/index.html'));
+});
+
+io.on('connect', function (socket) {
+  console.log("Houston, we have a connection");
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
